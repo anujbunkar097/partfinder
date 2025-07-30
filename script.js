63a7428d460a842cd29c0019808026145282f3c8
anujbunkar097 <anuj.bunkar@transformco.com>
2025-07-30 12:51:54 -0500

    Update script.js

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ async function searchParts() {
     }
 
     // IMPORTANT: Replace this with your actual n8n Webhook URL
-    const webhookUrl = 'YOUR_N8N_WEBHOOK_URL_GOES_HERE';
+    const webhookUrl = 'https://transformco.app.n8n.cloud/webhook-test/0299c781-9597-4a44-8efa-73ccf75bf26a';
 
     resultsContainer.innerHTML = '';
     loader.style.display = 'block';
